1b10ec53123d40a37a81695841f377b9fce07651
Chowa <arfinchowa524@gmail.com>
2025-07-18 00:05:28 +0600

    truthy, falsy, Ternary operator, shortcut and or

---
+++ shortcut.js
@@ -0,0 +1,17 @@
+// '' , 0, false , null, undefined
+// 'almas,5, true,{},[]
+let myVar =5;
+if(myVar){
+myVar =myVar*100
+}else{
+    myVar=0;
+}
+let myMoney =50;
+if(!myMoney){
+
+}
+
+let food =money>100?'biriany' :'cha biscuit';
+
+const isActive =true;
+isActive =!isActive
