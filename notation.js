0e3edcca2b0a14c6c214f80ee644dc6ce306d681
Chowa <arfinchowa524@gmail.com>
2025-07-18 13:04:36 +0600

    react started

---
+++ notation.js
@@ -0,0 +1,19 @@
+const person={
+    name:'chowa',
+    proyfessioyn:'student',
+    age:369,
+    25 : 'summer',
+    address:'shahabag'
+
+}
+const prof1 =person.proyfesson;
+
+// bracgket noytatioyn
+
+const prof2 =person["profession"]
+const pName ='profession';
+const prof3 =person[pName];
+console.log(prof2)
+
+const season = person[25];
+// for in loop property name will be handled
