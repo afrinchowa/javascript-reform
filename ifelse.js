8d43bf10805192879b2dfe72855b9dacb1a60ff9
Chowa <arfinchowa524@gmail.com>
2025-10-10 22:40:39 +0600

    switch case added

---
+++ ifelse.js
@@ -0,0 +1,7 @@
+if (condition1) {
+  // code to execute if condition1 is true
+} else if (condition2) {
+  // code to execute if the condition1 is false and condition2 is true
+} else {
+  // code to execute if the condition1 is false and condition2 is false
+}
