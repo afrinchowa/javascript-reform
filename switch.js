832a3f107c7488cebca7967f6a15ea600eb9aeff
Chowa <arfinchowa524@gmail.com>
2025-10-13 23:03:48 +0600

    ternery and switch case added

--- switch.js
+++ switch.js
@@ -1,10 +1,39 @@
-switch(expression) {
-  case x:
-    // code block
+// switch(expression) {
+//   case x:
+//     // code block
+//     break;
+//   case y:
+//     // code block
+//     break;
+//   default:
+//     // code block
+// }
+
+// Example
+// The getDay() method returns the weekday as a number between 0 and 6.
+
+// (Sunday=0, Monday=1, Tuesday=2 ..)
+
+// This example uses the weekday number to calculate the weekday name:
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
     break;
-  case y:
-    // code block
+  case 1:
+    day = "Monday";
     break;
-  default:
-    // code block
+  case 2:
+     day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
 }
---
+++ ternary.js
@@ -0,0 +1,10 @@
+let text = (age < 18) ? "Minor" : "Adult";
+// let isMember = true;
+// let discount = isMember ? 0.2 : 0;
+// let isMember = false;
+// let discount = isMember ? 0.2 : 0;
+// The conditional operator is a shorthand for writing conditional if...else statements.
+
+// It is called a ternary operator because it takes three operands.
+Syntax
+(condition) ? expression1 : expression2
