c7afc44ce4e7c3397b6be2e373593c00b92631c6
Chowa <arfinchowa524@gmail.com>
2025-10-16 10:37:44 +0600

    added the number method

---
+++ Numbers/method.jsx
@@ -0,0 +1,17 @@
+// Basic Methods
+// Basic number methods can be used on any number:
+
+// toString()
+// toExponential()
+// toFixed()
+// toPrecision()
+// valueOf()
+// Static Methods
+// Static methods can only be used on Number:
+
+// Number.isFinite()
+// Number.isInteger()
+// Number.isNan()
+// Number.isSafeInteger()
+// Number.parseInt()
+// Number.parseFloat()
