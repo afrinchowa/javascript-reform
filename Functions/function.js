a1b38648b22dd3ee23a65dff98e3b2b9b121c1df
Chowa <arfinchowa524@gmail.com>
2025-10-17 23:38:01 +0600

    added the function

---
+++ Functions/function.js
@@ -0,0 +1,14 @@
+// What are Functions?
+// Functions are fundamental building blocks in all programming.
+
+// Functions enable better code organization, modularity, and efficiency.
+
+// Functions are reusable block of code designed to perform a particular task.
+
+// Functions execute when they are "called" or "invoked".
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+console.log(result)
