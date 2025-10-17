837d6973e8deb2947dbe0b71cd4e591a81aff20b
Chowa <arfinchowa524@gmail.com>
2025-10-18 00:05:57 +0600

    added the function syntax

---
+++ Functions/javaScriptFunctionSyntax.js
@@ -0,0 +1,10 @@
+// A function is defined with the function keyword, followed by the function name, followed by parentheses ( ), followed by brackets { }.
+
+// The name follows the naming rules for variables (letters, digits, ...).
+
+// Optional parameters are listed inside parentheses: (p1, p2, p3)
+
+// The code to be executed is listed inside curly brackets: { code }
+
+// Functions can optionally return a value back to the "caller".
+
